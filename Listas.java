927eb40e76e34b8e65e7484f67b6027fe52401c1
ShofiSo <kelly.rabanales@gmail.com>
2025-03-31 21:06:38 -0600

    Tercer Commit
    Creando la lista

--- Listas.java
+++ Listas.java
@@ -7,3 +7,18 @@ class Node {
         this.next = null;
     }
 }
+class LinkedList {
+    private Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
