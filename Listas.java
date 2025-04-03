2316a5d05085a03ef6534d3f0f3c721d4539eb07
ShofiSo <kelly.rabanales@gmail.com>
2025-04-02 22:12:49 -0600

    Onceavo Commit
    Pues ahí vamos....

--- Listas.java
+++ Listas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Node {
     int data;
     Node next;
@@ -8,19 +10,11 @@ class Node {
     }
 }
 
-class SimpleList {
-    private Node head;
+class LinkedList {
+    Node head;
 
-    public void add(int data) {
-        if (head == null) {
-            head = new Node(data);
-            return;
-        }
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = new Node(data);
+    public LinkedList() {
+        this.head = null;
     }
 
     public void addFirst(int data) {
@@ -29,19 +23,35 @@ class SimpleList {
         head = newNode;
     }
 
-    public void addMiddle(int data, int position) {
-        if (position <= 0) {
-            addFirst(data);
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
         }
         Node current = head;
-        for (int i = 0; i < position - 1 && current != null; i++) {
+        while (current.next != null) {
             current = current.next;
         }
-        if (current == null) return;
-        Node newNode = new Node(data);
-        newNode.next = current.next;
         current.next = newNode;
+    }
+
+    public void addMiddle(int data, int position) {
+        if (position <= 0 || head == null) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node current = head;
+        int count = 0;
+        while (current != null && count < position - 1) {
+            current = current.next;
+            count++;
+        }
+        if (current != null) {
+            newNode.next = current.next;
+            current.next = newNode;
+        }
     }
 
     public void remove(int data) {
@@ -69,7 +79,9 @@ class SimpleList {
     }
 
     public void reverse() {
-        Node prev = null, current = head, next;
+        Node prev = null;
+        Node current = head;
+        Node next = null;
         while (current != null) {
             next = current.next;
             current.next = prev;
@@ -82,26 +94,52 @@ class SimpleList {
     public boolean contains(int value) {
         Node current = head;
         while (current != null) {
-            if (current.data == value) return true;
+            if (current.data == value) {
+                return true;
+            }
             current = current.next;
         }
         return false;
     }
+}
 
-    public static void Listas(String[] args) {
-        SimpleList list = new SimpleList();
+public class Listas {  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+
         list.addFirst(1);
         list.add(3);
         list.addMiddle(5, 1);
+        System.out.println("Lista actual:");
         list.printList();
+        
         list.remove(3);
+        System.out.println("Lista después de eliminar el 3:");
         list.printList();
+        
         list.add(7);
-        System.out.println("Contains 5: " + list.contains(5));
-        System.out.println("Contains 9: " + list.contains(9));
+        System.out.println("¿La lista contiene el 5? " + list.contains(5));
+        System.out.println("¿La lista contiene el 9? " + list.contains(9));
+
         list.reverse();
+        System.out.println("Lista después de revertir:");
         list.printList();
+
         list.addFirst(9);
+        System.out.println("Lista final:");
         list.printList();
+
+        // Solicitar DNI al usuario en la consola
+        System.out.print("\nIngrese su DNI por favor: ");
+        int dni = scanner.nextInt();
+
+        if (list.contains(dni)) {
+            System.out.println("El DNI " + dni + " esta en la lista");
+        } else {
+            System.out.println("El DNI " + dni + " NO esta en la lista");
+        }
+
+        scanner.close();
     }
 }
